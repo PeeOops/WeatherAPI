c3fc6ba6ff3c6ee9135128fe679785cbb4d3dc66
PeeOops <liem.richiee@gmail.com>
2025-05-05 16:11:19 +0700

    Added JS variables

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const addCityButton = document.getElementById("add-city");
+const cityList = document.getElementById("cities");
+const content = document.getElementById("content");
+const sevenDayForecasts = document.getElementById("forecasts");
