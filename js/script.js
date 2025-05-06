0f2c3628c79d5d2b99b1bf96febdb61734d5f9bb
PeeOops <liem.richiee@gmail.com>
2025-05-06 11:29:44 +0700

    Added defaultCity

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const addCityButton = document.getElementById("add-city");
 const cancelAddCityButton = document.getElementById("cancel");
+const modal = document.getElementById("modal");
+const cityInput = document.getElementById("city");
 const cityList = document.getElementById("cities");
 const content = document.getElementById("content");
 const sevenDayForecasts = document.getElementById("forecasts");
-const modal = document.getElementById("modal");
 
+
+
+// Modal
 addCityButton.addEventListener("click", () => {
     modal.showModal();
 })
@@ -12,4 +16,29 @@ addCityButton.addEventListener("click", () => {
 cancelAddCityButton.addEventListener("click", () => {
     modal.close();
 })
+
+// Default City List
+const defaultCity = [
+    {
+        id: 1,
+        city: "Berlin",
+        country: "Germany",
+        lat: 52.5108850,
+        lon: 13.3989367
+    },
+    {
+        id: 2,
+        city: "Jakarta",
+        country: "Indonesia",
+        lat: 52.5108850,
+        lon: 13.3989367
+    },
+]
+
+// Set localStorage
+if(!localStorage.getItem("cities")){
+    localStorage.setItem("cities", JSON.stringify(defaultCity));
+}
+
+
 
