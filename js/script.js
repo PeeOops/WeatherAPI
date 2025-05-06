713525603c2261aec03960f5dabeea1ed69d07f1
PeeOops <liem.richiee@gmail.com>
2025-05-06 11:10:46 +0700

    Added Add City Modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 const addCityButton = document.getElementById("add-city");
+const cancelAddCityButton = document.getElementById("cancel");
 const cityList = document.getElementById("cities");
 const content = document.getElementById("content");
 const sevenDayForecasts = document.getElementById("forecasts");
+const modal = document.getElementById("modal");
+
+addCityButton.addEventListener("click", () => {
+    modal.showModal();
+})
+
+cancelAddCityButton.addEventListener("click", () => {
+    modal.close();
+})
+
