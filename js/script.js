59bbab4d8696fc013ea971e99c7f27f8a7a5c8e2
PeeOops <liem.richiee@gmail.com>
2025-05-06 11:57:01 +0700

    Adding fetch from nominatim geoAPI

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const addCityButton = document.getElementById("add-city");
 const cancelAddCityButton = document.getElementById("cancel");
+const submitAddCityButton = document.getElementById("submit");
 const modal = document.getElementById("modal");
 const cityInput = document.getElementById("city");
 const cityList = document.getElementById("cities");
@@ -40,5 +41,29 @@ if(!localStorage.getItem("cities")){
     localStorage.setItem("cities", JSON.stringify(defaultCity));
 }
 
+// Add City
+submitAddCityButton.addEventListener("click", async () => {
+    const city = cityInput.value.toLowerCase();
+
+    if(!city){
+        alert("Please check your input");
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1`);
+
+        if(!response.ok){
+            throw new Error(`Error : ${response.status}`)
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log(`Fetch failed:`, error.message);
+    }
+
+})
 
 
